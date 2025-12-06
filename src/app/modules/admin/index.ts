7e47c67614f3a5af16ffcdcc496f36ff22af7204
rashedul-dev <rashedulislam.edge@gmail.com>
2025-12-06 22:26:26 +0600

    feat(admin): export admin module

---
+++ src/app/modules/admin/index.ts
@@ -0,0 +1,9 @@
+// ============================================================================
+// FILE: backend/src/app/modules/admin/index.ts
+// ============================================================================
+
+export { AdminController } from "./admin.controller";
+export { AdminService } from "./admin.service";
+export { default as adminRoutes } from "./admin.routes";
+export * from "./admin.constant";
+export * from "./admin.interface";
