bea710825dfc0222f0cb970a5355bfba0d702497
rashedul-dev <rashedulislam.edge@gmail.com>
2025-12-06 22:27:08 +0600

    feat(notification): export notification module

---
+++ src/app/modules/notification/index.ts
@@ -0,0 +1,9 @@
+// ============================================================================
+// FILE: backend/src/app/modules/notification/index.ts
+// ============================================================================
+
+export { NotificationController } from "./notification.controller";
+export { NotificationService } from "./notification.service";
+export { default as notificationRoutes } from "./notification.routes";
+export * from "./notification.constant";
+export * from "./notification.interface";
